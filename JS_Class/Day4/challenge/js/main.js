1a1a9060e1546ffbbc03494c5458e55ca201c194
Tri Ng <tringuyencccm@gmail.com>
2022-11-09 21:42:26 -0800

    Made names shorter

--- JS_Class/Day4/challenge/js/main.js
+++ JS_Class/Day4/challenge/js/main.js
@@ -1,14 +1,25 @@
 let q1 = prompt('Please input a number:');
 let q2 = prompt('Please input another number:');
 
-
-
-if (q1 < q2) {
-    alert("Your first number is smaller.");
-} else if (q1 === q2){
-    alert('Two numbers are equal.');
-} else if (q1 > q2) {
-    alert('Your first number is bigger')
-} else {
-    console.log('Are you sure this is a number?')
+function numbertest(q1, q2) {
+    if (q1 < q2) {
+        // alert("Your first number is smaller."); Failed attempt
+        alert(parseFloat(q1)+ ' is smaller than ' + parseFloat(q2));
+    } else if (q1 === q2){
+        alert(parseFloat(q1)+ ' is equal ' + parseFloat(q2));
+    } else if (q1 > q2) {
+        alert(parseFloat(q1)+ ' is larger than ' + parseFloat(q2));
+    }
+    else {
+        console.log('Idk');
+    }
 }
+function compare(){
+    if (isNaN(q1, q2)) {
+        alert('PLEASE! Give me a real number *stare*:');
+        recurse(question);
+    } else {
+        numbertest(q1, q2);
+    }
+}
+compare();
